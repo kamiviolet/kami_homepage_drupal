2c21d8f394e20a8d81d0a8aa13d67dd06e254a03
kakeilam <kami@spokenink.co.uk>
2025-05-26 18:08:20 +0100

    rename custom bootstrap subtheme as bootstrap_uranus, move it out of bootstrap folder, reset bootstrap_subtheme as default

--- web/themes/contrib/bootstrap/subthemes/bootstrap_subtheme/js/global.js
+++ web/themes/contrib/bootstrap/subthemes/bootstrap_subtheme/js/global.js
@@ -7,27 +7,6 @@
   'use strict';
 
   Drupal.behaviors.bootstrap_subtheme = {
-    attach: function (context, settings) {
-      console.log('context');
-      console.log(context);
-      console.log('settings');
-      console.log(settings);
-
-      context.querySelectorAll('.navbar-toggler').forEach(function (element) {
-        element.innerHTML = `
-            <div class="bar"></div>
-            <div class="bar"></div>
-            <div class="bar"></div>
-        `;
-        element.addEventListener('click', function (event) {
-          let targetId = event.target.closest('.navbar-toggler')?.dataset['target'];
-          let targetEl = document.querySelector(targetId);
-          if (targetEl) {
-            targetEl.classList.toggle('collapse');
-            event.target.closest('.navbar-toggler').classList.toggle('is-expanded');
-         }
-        })
-      })
-    },
+    attach: function (context, settings) {},
   };
 })(Drupal);
