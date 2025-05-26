3fe832d94f8743489e8baf1958ccfae8662b660c
kakeilam <kami@spokenink.co.uk>
2025-05-26 17:00:59 +0100

    initial commit

---
+++ web/themes/contrib/bootstrap/subthemes/bootstrap_subtheme/js/global.js
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.bootstrap_subtheme = {
+    attach: function (context, settings) {
+      console.log('context');
+      console.log(context);
+      console.log('settings');
+      console.log(settings);
+
+      context.querySelectorAll('.navbar-toggler').forEach(function (element) {
+        element.innerHTML = `
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        `;
+        element.addEventListener('click', function (event) {
+          let targetId = event.target.closest('.navbar-toggler')?.dataset['target'];
+          let targetEl = document.querySelector(targetId);
+          if (targetEl) {
+            targetEl.classList.toggle('collapse');
+            event.target.closest('.navbar-toggler').classList.toggle('is-expanded');
+         }
+        })
+      })
+    },
+  };
+})(Drupal);
